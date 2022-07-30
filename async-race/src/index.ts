32be27deb62baa69c0e50aef70ea9c12405db10e
Egor Tabunov <Vonubat@gmail.com>
2022-07-30 23:36:46 +0300

    feat: add get-random-color utility

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,2 +1,5 @@
 import 'normalize.css';
 import './scss/styles.scss';
+import getRandomColor from './utilities/get-random-color';
+
+getRandomColor();
---
+++ async-race/src/utilities/get-random-color.ts
@@ -0,0 +1,13 @@
+const getRandomColor = () => {
+  const RGB_MAX_VALUE = 255;
+  const rgbValueGenerator: () => number = (): number => Math.ceil(Math.random() * RGB_MAX_VALUE);
+  const R: number = rgbValueGenerator();
+  const G: number = rgbValueGenerator();
+  const B: number = rgbValueGenerator();
+  const randomColor = `#${R.toString(16)}${G.toString(16)}${B.toString(16)}`;
+  // console.log(randomColor);
+
+  return randomColor;
+};
+
+export default getRandomColor;
